f1f48b8df0a47f4cc2ed7415501a211b59d13322
BenAttenborough <benattenborough@BenAttenboroughs-MacBook-Pro.local>
2024-11-17 18:04:59 +0000

    Work on update

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ interface Model {
   }
 }
 
-const model: Model =
+let model: Model =
 {
   lives: 0,
   keysPressed: {
@@ -43,25 +43,30 @@ const model: Model =
 function update(tFrame = 0) {
   const delta = tFrame - lastTick;
   lastTick = tFrame;
-  // handleKeyboardInput(MyGame, delta);
+  model = states[playState.id].update(model, delta)
 }
 
 class State {
   id: string;
   draw: (canvas: HTMLCanvasElement) => void;
+  update: (m: Model, delta: number) => Model;
 
   constructor(config) {
     this.id = config.id;
     this.draw = config.draw;
+    this.update = config.update;
   }
 }
 
 const titleState = new State(
   {
-    id: "tit",
+    id: "title",
     draw: (canvas) => {
       const ctx = canvas.getContext("2d");
       ctx.fillText("TITLE STATE", 100, 18);
+    },
+    update: (m, delta) => {
+      return m;
     }
   }
 );
@@ -76,7 +81,15 @@ const playState = new State(
       ctx.fillRect(0, 0, 50, 50);
       ctx.fillRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = "#FFFFFF";
-      ctx.fillText("MAIN MENU", 100, 18);
+      ctx.fillText("PLAY STATE " + model.lives, 100, 18);
+    },
+    update: (m, delta) => {
+      // console.log("update");
+      if (m.keysPressed.ArrowUp) {
+        // console.log("Up pressed")
+        m.lives = m.lives + 1;
+      }
+      return m;
     }
   }
 );
@@ -127,18 +140,22 @@ function keyboardInput() {
 document.onreadystatechange = () => {
   if (document.readyState === "complete") {
     // document ready
-    // keyboardInput();
+    keyboardInput();
 
     (() => {
       function main(tFrame?: number) {
+        // console.log("FF")
         // MyGame.stopMain = window.requestAnimationFrame(main);
+        window.requestAnimationFrame(main);
 
         update(tFrame); // Call your update method. In our case, we give it rAF's timestamp.
-        if (MyGame.stateChanged) {
-          console.log("State change")
-          states[playState.id].draw(MyGame.canvas)
-        }
-        MyGame.stateChanged = false;
+        // if (MyGame.stateChanged) {
+        //   console.log("State change")
+        //   states[playState.id].draw(MyGame.canvas)
+        // }
+        // MyGame.stateChanged = false;
+        // console.log("State change")
+        states[playState.id].draw(MyGame.canvas)
       }
 
       main(); // Start the cycle
